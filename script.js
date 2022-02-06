d1fe64ae228b956b17fe3c5ddcf978edaa28bc87
rrafael <rsc250505@gmail.com>
2022-02-05 23:00:49 -0300

    Add function that changes squares color

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
+let container = document.querySelector(".container");
 
 function generateSquares() {
-    for (var i = 0; i < 16 * 16; i++) {
-        let square = document.createElement('div')
-        let container = document.querySelector('.container')
-        square.classList.add('square')
-        container.appendChild(square)
-    }
+  for (var i = 0; i < 16 * 16; i++) {
+    let square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
+  }
 }
 
-generateSquares()
+generateSquares();
+
+function changeSquareColor(e) {
+  e.target.style.backgroundColor = "black";
+}
+
+container.addEventListener("mousemove", (e) => {
+  changeSquareColor(e);
+});
