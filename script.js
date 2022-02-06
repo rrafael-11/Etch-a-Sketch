9b440100fb569de104cfed561fd94c7c27b51c58
Rafael <rsc250505@gmail.com>
2022-02-06 18:06:34 -0300

    Update generateSquares function and add event listener to input-range

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
 let container = document.querySelector(".container");
-let squares = () => {return document.querySelectorAll('.square')}
-let inputColor = () => {return document.querySelector('#inputColor').value}
+let inputGrid = document.querySelector("#gridSize");
+let currentGridTxt = document.querySelector("#currentGridTxt");
+let squares = () => { return document.querySelectorAll(".square"); };
+let gridSize = () => { return document.querySelector("#gridSize").value; };
+let inputColor = () => { return document.querySelector("#inputColor").value; };
 
+updateGrid(gridSize());
 
-function generateSquares() {
-  for (var i = 0; i < 16 * 16; i++) {
+function updateGrid(gridSize) {
+  resetGrid();
+  container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+  container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  for (var i = 0; i < gridSize * gridSize; i++) {
     let square = document.createElement("div");
     square.classList.add("square");
     container.appendChild(square);
   }
 }
 
-generateSquares();
-
-function changeSquareColor(e) {
-  e.target.style.backgroundColor = inputColor();
+function resetGrid() {
+  while (container.lastChild) {
+    container.lastChild.remove();
+  }
 }
 
-squares().forEach(square => (
-  square.addEventListener('mousemove', (e) => {changeSquareColor(e)})
-))
+function updateGridText() {
+  return (currentGridTxt.textContent = `${gridSize()}x${gridSize()}`);
+}
+
+function changeSquareColor(e) {
+  return (e.target.style.backgroundColor = inputColor());
+}
+
+squares().forEach((square) => {
+  square.addEventListener("mousemove", (e) => {
+    changeSquareColor(e);
+  });
+});
+
+inputGrid.addEventListener("mousedown", () => {
+  inputGrid.onmousemove = updateGridText;
+  inputGrid.onmouseup = () => { 
+    updateGrid(gridSize()); 
+  };
+});
